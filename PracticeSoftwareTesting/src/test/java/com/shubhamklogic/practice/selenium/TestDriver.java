95a878d5cbff4937e95383f89d3c28cdcc6e8a04
srishubham2 <srishubham58@gmail.com>
2022-09-17 17:26:06 +0530

    Added Main class to run Test Cases

---
+++ PracticeSoftwareTesting/src/test/java/com/shubhamklogic/practice/selenium/TestDriver.java
@@ -0,0 +1,10 @@
+package com.shubhamklogic.practice.selenium;
+
+public class TestDriver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Dummy entry
+	}
+
+}
